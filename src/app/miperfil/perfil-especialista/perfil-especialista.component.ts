a1af43d3421298191689ad5d8137830591571665
hernanVilar <hernannvilar@gmail.com>
2023-03-11 02:03:48 -0300

    datosagregados

--- src/app/miperfil/perfil-especialista/perfil-especialista.component.ts
+++ src/app/miperfil/perfil-especialista/perfil-especialista.component.ts
@@ -111,6 +111,7 @@ export class PerfilEspecialistaComponent implements OnInit {
       }
     })
     //asd
+    //asda
     // let data = (<HTMLInputElement>document.getElementById('agregar')).value;
     
     
